adddaef345667597b71b479f57124b084b1902d3
clyfe <cliff.kamau@student.moringaschool.com>
2023-07-22 09:57:39 +0300

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Question from "./Question";
+import Timer from "./Timer"; // New Timer component
 import quiz from "../data/quiz";
 
 function App() {
-  const [questions, setQuestions] = useState(quiz);
-  const [currentQuestionId, setCurrentQuestion] = useState(1);
+  const [currentQuestionId, setCurrentQuestionId] = useState(1);
   const [score, setScore] = useState(0);
-  const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+  const currentQuestion = quiz.find((q) => q.id === currentQuestionId);
+  const [timeRemaining, setTimeRemaining] = useState(10);
 
-  function handleQuestionAnswered(correct) {
-    if (currentQuestionId < questions.length) {
-      setCurrentQuestion((currentQuestionId) => currentQuestionId + 1);
-    } else {
-      setCurrentQuestion(null);
+  const handleQuestionAnswered = useCallback(
+    (correct) => {
+      if (correct) {
+        setScore((score) => score + 1);
+      }
+      setCurrentQuestionId((currentQuestionId) => currentQuestionId + 1);
+      setTimeRemaining(10); // Reset timeRemaining after each question
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (timeRemaining === 0 && currentQuestionId !== null) {
+      setCurrentQuestionId(null);
     }
-    if (correct) {
-      setScore((score) => score + 1);
+
+    if (currentQuestionId !== null) {
+      const timerId = setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+
+      return () => {
+        clearTimeout(timerId);
+      };
     }
-  }
+  }, [timeRemaining, currentQuestionId]);
 
   return (
-    <main>
-      <section>
-        {currentQuestion ? (
-          <Question
-            question={currentQuestion}
-            onAnswered={handleQuestionAnswered}
-          />
-        ) : (
-          <>
-            <h1>Game Over</h1>
-            <h2>Total Correct: {score}</h2>
-          </>
-        )}
-      </section>
-    </main>
+    <div>
+      <main>
+        <section>
+          {currentQuestionId !== null ? (
+            <>
+              <Question question={currentQuestion} onAnswered={handleQuestionAnswered} />
+              <Timer timeRemaining={timeRemaining} /> {/* Display the timer */}
+            </>
+          ) : (
+            <>
+              <h1>Game Over</h1>
+              <h2>Total Correct: {score}</h2>
+            </>
+          )}
+        </section>
+      </main>
+    </div>
   );
 }
 
---
+++ src/components/Timer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Timer({ timeRemaining }) {
+  return <h1>Time Remaining: {timeRemaining} seconds</h1>;
+}
+
+export default Timer;
